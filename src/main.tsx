fac891d35abf7209db23e739f96cd5727b6501ac
Charles Lowell <cowboyd@frontside.com>
2024-03-21 10:55:06 -0500

    delinquent backend

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,63 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  NavLink,
+  Outlet,
+  RouterProvider,
+  useLoaderData,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Nav />,
+    loader: async() => {
+      const response = await fetch("/api/v2/episodes");
+      const episodes = await response.json();
+      return episodes.slice(0,10);
+      
+    },
+    children: [
+      {
+        path: "/:slug",
+        element: <Episode />,
+        loader: async ({ params }) => {
+	  const response = await fetch(`/api/v2/episode/${params.slug}`);
+	  return await response.json();
+	},
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+type EpisodeMeta = {
+  slug: string;
+  title: string;
+}
+
+export function Nav() {
+  const data = useLoaderData() as EpisodeMeta[];
+  return (
+    <div>
+      <ul>{data.map(episode => <li key={episode.slug}>
+				 <NavLink to={episode.slug}>{episode.title}</NavLink>
+			       </li>)}</ul>
+      <Outlet />
+    </div>
+  );
+}
+
+export function Episode() {
+  const data = useLoaderData() as any;
+  return <article>{data.transcript}</article>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+	target: `https://www.learnwithjason.dev`,
+	changeOrigin: true,
+	configure(proxy, options) {
+	  let original = proxy.web;
+	  proxy.web = function(req, res) {
+	    setTimeout(() => {
+	      original.call(proxy, req, res);
+	    }, 2000);
+	  }
+	  //	  console.log(proxy.proxyRequest.toString(), options);
+	},
+      },      
+    }
+  }
 })
